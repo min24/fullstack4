3f864c078a6e2b03419143d34fded4a31f1a5405
MinMin <nguyenduy0204.xy@gmail.com>
2020-08-15 03:43:17 +0300

    2020-08-15

---
+++ npm/webpack-demo/src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+
+function component() {
+    const element = document.createElement('div');
+
+    // Lodash now import by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
+
+
+
+console.log("fxckhvjgh");
